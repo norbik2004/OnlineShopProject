a9ca820f0dc68c6b75658efba62e2fa5f939a7f7
Norbert <kieler103@gmail.com>
2025-01-02 13:06:52 +0100

    #feat: new AccountController, LoginView and _AccountLayout

---
+++ OnlineShopProject/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
