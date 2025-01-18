7b966c76c0579f50adcdcd5bd667be9999f58928
Norbert <kieler103@gmail.com>
2025-01-18 10:43:17 +0100

    #fix: Added footer to Layouts, #feat: Admin can see product Details

---
+++ OnlineShopProject/Services/ViewModels/Admin/AdminProductDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopProject.Services.ViewModels.Admin
+{
+    public class AdminProductDetailsViewModel
+    {
+        public long ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public string IMGFileLink { get; set; } = string.Empty;
+        public Category Category { get; set; }
+    }
+}
